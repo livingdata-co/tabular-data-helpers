8f4163828bb500f1e1a1340528d059550458a5df
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2022-09-10 22:57:14 +0200

    Add createAutoDecodeStream

---
+++ test/auto-decode-stream.js
@@ -0,0 +1,42 @@
+import test from 'ava'
+import getStream from 'get-stream'
+import intoStream from 'into-stream'
+import iconv from 'iconv-lite'
+
+import {createAutoDecodeStream} from '../lib/auto-decode-stream.js'
+
+test('createAutoDecodeStream / ISO-8859-1', async t => {
+  let encoding
+
+  const autoDecodeStream = createAutoDecodeStream({
+    onEncoding(detectedEncoding) {
+      encoding = detectedEncoding
+    }
+  })
+
+  const decodedString = await getStream(
+    intoStream(iconv.encode('éléphant', 'ISO-8859-1'))
+      .pipe(autoDecodeStream)
+  )
+
+  t.is(encoding, 'ISO-8859-1')
+  t.is(decodedString, 'éléphant')
+})
+
+test('createAutoDecodeStream / UTF-8', async t => {
+  let encoding
+
+  const autoDecodeStream = createAutoDecodeStream({
+    onEncoding(detectedEncoding) {
+      encoding = detectedEncoding
+    }
+  })
+
+  const decodedString = await getStream(
+    intoStream(iconv.encode('éléphant', 'UTF-8'))
+      .pipe(autoDecodeStream)
+  )
+
+  t.is(encoding, 'UTF-8')
+  t.is(decodedString, 'éléphant')
+})
